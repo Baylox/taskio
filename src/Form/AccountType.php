0471d1e1cf68e2fdcf67ec01a41f040adb3ec037
Baylo <microsoftbaylo@laposte.net>
2025-09-05 10:42:11 +0200

    feat: implement Account management functionality with CRUD operations

---
+++ src/Form/AccountType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use App\Entity\Board;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('role')
+            ->add('isVerified')
+            ->add('name')
+            ->add('lastname')
+            ->add('boards', EntityType::class, [
+                'class' => Board::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
