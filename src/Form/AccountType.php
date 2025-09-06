9c0a8366eb933eae24f16c80e0a458e0cbbff765
Baylo <microsoftbaylo@laposte.net>
2025-09-06 15:26:55 +0200

    feat: enhance AccountType form by adding email, name, and lastname fields with appropriate attributes

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -2,28 +2,42 @@
 
 namespace App\Form;
 
-use App\Entity\Account;
 use App\Entity\Board;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Account;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
 
 class AccountType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'autocomplete' => 'email',
+                    'inputmode' => 'email', // Mobile-friendly keyboard
+                    'placeholder' => 'votre@email.com'
+                ]
+            ])
+            ->add('name', TextType::class, [
+                'required' => false,
+                'label'    => 'First name',
+                'attr'     => ['autocomplete' => 'given-name'],
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => false,
+                'label'    => 'Last name',
+                'attr'     => ['autocomplete' => 'family-name'],
+            ])
             ->add('role')
-            ->add('isVerified')
-            ->add('name')
-            ->add('lastname')
-            ->add('boards', EntityType::class, [
-                'class' => Board::class,
-                'choice_label' => 'id',
-                'multiple' => true,
+            ->add('isVerified', CheckboxType::class, [
+                'disabled' => true,   // greyed out, cannot be changed
+                'label'    => 'Email vérifié',
             ])
         ;
     }
