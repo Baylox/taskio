df6cd8da9b6d45699d5409350a06decbe07aeef4
Baylo <microsoftbaylo@laposte.net>
2025-09-07 16:31:09 +0200

    feat: update routing and access control for account management actions

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,15 +6,16 @@ use App\Entity\Account;
 use App\Form\AccountType;
 use App\Repository\AccountRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/account')]
+#[Route('/account', name: 'app_account_')]
 final class AccountController extends AbstractController
 {
-    #[Route(name: 'app_account_index', methods: ['GET'])]
+    #[Route(name: 'index', methods: ['GET'])]
     public function index(AccountRepository $accountRepository): Response
     {
         return $this->render('account/index.html.twig', [
@@ -22,35 +23,16 @@ final class AccountController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_account_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $account = new Account();
-        $form = $this->createForm(AccountType::class, $account);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($account);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_account_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('account/new.html.twig', [
-            'account' => $account,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_account_show', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Account $account): Response
     {
         return $this->render('account/show.html.twig', [
             'account' => $account,
         ]);
     }
-
-    #[Route('/{id}/edit', name: 'app_account_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Account $account, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(AccountType::class, $account);
@@ -67,8 +49,8 @@ final class AccountController extends AbstractController
             'form' => $form,
         ]);
     }
-
-    #[Route('/{id}', name: 'app_account_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Account $account, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$account->getId(), $request->getPayload()->getString('_token'))) {
