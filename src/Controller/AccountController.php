fcf6a99b75b8ac593e41fe699cbf98ee6ca211e8
Baylo <microsoftbaylo@laposte.net>
2025-09-09 16:27:52 +0200

    refactor: remove redundant ROLE_ADMIN annotations from AccountController methods

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -11,11 +11,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 #[IsGranted('ROLE_ADMIN')]
 #[Route('/account', name: 'app_account_')]
 final class AccountController extends AbstractController
 {
-    #[IsGranted('ROLE_ADMIN')]
     #[Route(name: 'index', methods: ['GET'])]
     public function index(AccountRepository $accountRepository): Response
     {
@@ -24,7 +24,6 @@ final class AccountController extends AbstractController
         ]);
     }
 
-    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Account $account): Response
     {
@@ -33,7 +32,6 @@ final class AccountController extends AbstractController
         ]);
     }
 
-    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Account $account, EntityManagerInterface $entityManager): Response
     {
@@ -42,7 +40,6 @@ final class AccountController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-
             return $this->redirectToRoute('app_account_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -52,7 +49,6 @@ final class AccountController extends AbstractController
         ]);
     }
 
-    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Account $account, EntityManagerInterface $entityManager): Response
     {
