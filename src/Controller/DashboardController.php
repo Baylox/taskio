4790ee632a568a2d811c46c2df9ed468bd17efd6
Baylo <microsoftbaylo@laposte.net>
2025-09-16 09:46:53 +0200

    refactor: remove showBoard method and streamline dashboard access

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,16 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Board;
+use App\Repository\BoardRepository;
 
+
+#[Route('/dashboard', name: 'app_dashboard', methods: ['GET'])]
 final class DashboardController extends AbstractController
 {
-    #[Route('/boards/{id}', name: 'app_board_dashboard', methods: ['GET'])]
-    public function showBoard(Board $board): Response
+    public function __invoke(BoardRepository $boards): Response
     {
-
-
-        return $this->render('dashboard/board.html.twig', [
-            'board' => $board,
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        // Ideally: filter by user's membership
+        $userBoards = $boards->findForUser($this->getUser());
+        return $this->render('dashboard/index.html.twig', [
+            'boards' => $userBoards,
         ]);
     }
 }
