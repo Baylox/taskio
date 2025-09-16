4f54342d4935b2c5d765b091da50e0458cf034c3
Baylo <microsoftbaylo@laposte.net>
2025-09-16 16:29:25 +0200

    refactor: simplify DashboardController by removing unused dependencies and methods

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,20 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Board;
-use App\Repository\BoardRepository;
 
-
-#[Route('/dashboard', name: 'app_dashboard', methods: ['GET'])]
 final class DashboardController extends AbstractController
 {
-    public function __invoke(BoardRepository $boards): Response
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_USER');
-        // Ideally: filter by user's membership
-        $userBoards = $boards->findForUser($this->getUser());
         return $this->render('dashboard/index.html.twig', [
-            'boards' => $userBoards,
+            'controller_name' => 'DashboardController',
         ]);
     }
 }
