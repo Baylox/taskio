3ccfb5e187e23801811cdc15af1760764e828fbb
Baylo <microsoftbaylo@laposte.net>
2025-09-17 17:48:04 +0200

    feat: implement lane editing functionality with modals and form handling

--- src/Controller/LaneController.php
+++ src/Controller/LaneController.php
@@ -7,6 +7,7 @@ use App\Entity\Board;
 use App\Form\LaneType;
 use App\Repository\LaneRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -25,7 +26,7 @@ final class LaneController extends AbstractController
 
     // PRG from the dashboard (adding a lane)
     // The Board is received in the URL to know where to attach the lane
-    #[Route('/boards/{id}/lanes/new', name: 'lane_new', methods: ['POST','GET'])]
+    #[Route('/boards/{id}/lanes/new', name: 'lane_new', methods: ['POST', 'GET'])]
     public function new(Board $board, Request $request, EntityManagerInterface $em, LaneRepository $repo): Response
     {
 
@@ -61,7 +62,7 @@ final class LaneController extends AbstractController
     #[Route('/{id}', name: 'app_lane_delete', methods: ['POST'])]
     public function delete(Request $request, Lane $lane, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$lane->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $lane->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($lane);
             $entityManager->flush();
         }
@@ -69,22 +70,47 @@ final class LaneController extends AbstractController
         return $this->redirectToRoute('app_lane_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    // Todo -> AJAXs
-    #[Route('/{id}/edit', name: 'app_lane_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Lane $lane, EntityManagerInterface $entityManager): Response
+    #[Route('/lanes/{id}/edit', name: 'lane_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Lane $lane, EntityManagerInterface $em): Response
     {
+        $board = $lane->getBoard();
+        //$this->denyAccessUnlessGranted('EDIT', $board);
+
         $form = $this->createForm(LaneType::class, $lane);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_lane_index', [], Response::HTTP_SEE_OTHER);
+            $em->flush();
+            return $this->redirectToRoute('app_board_dashboard', ['id' => $board->getId()]);
         }
 
-        return $this->render('dashboard/lane/edit.html.twig', [
-            'lane' => $lane,
-            'form' => $form,
+        // Errors → redisplay dashboard with the UPDATE modal for THIS lane open
+        $laneEditForms = [];
+        foreach ($board->getLanes() as $ln) {
+            $laneEditForms[$ln->getId()] = $ln->getId() === $lane->getId()
+                ? $form->createView() // keep errors
+                : $this->createForm(LaneType::class, $ln)->createView();
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+        'board'           => $board,
+        'laneForm'        => $this->createForm(LaneType::class)->createView(),
+        'laneEditForms'   => $this->buildLaneEditForms($board, $form, $lane),
+        'openEditLaneId'  => $lane->getId(),
         ]);
+    }
+
+    /** @return array<int*/
+    private function buildLaneEditForms(Board $board, ?FormInterface $currentForm = null, ?Lane $currentLane = null): array
+    {
+        $forms = [];
+        foreach ($board->getLanes() as $ln) {
+            if ($currentLane && $ln->getId() === $currentLane->getId() && $currentForm) {
+                $forms[$ln->getId()] = $currentForm->createView();
+            } else {
+                $forms[$ln->getId()] = $this->createForm(LaneType::class, $ln)->createView();
+            }
+        }
+        return $forms;
     }
 }
