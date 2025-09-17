591a5eeacb23febb7dc83d6d833f20c765fec0e3
Yolrie <quentin.gergaud44450@gmail.com>
2025-09-17 10:04:35 +0200

    feat: enhance Board form to include title validation and improved attributes

--- src/Form/BoardType.php
+++ src/Form/BoardType.php
@@ -8,20 +8,34 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class BoardType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title') // ajout message erreur si pas de titre
-            // Voir si il faut modifier le "add" de accounts car cela peut créer un nouv account ? (et non le lier a un existant)
-            /*->add('accounts', EntityType::class, [
-                'class' => Account::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])*/
-        ;
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label'    => 'Board title',
+                'attr'     => [
+                    'placeholder' => 'Enter a title',
+                    'class' => 'input input-bordered w-full text-base-content bg-base-100'
+                ],
+                'label_attr' => [
+                    'class' => 'label-text text-base-content font-medium'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'The title field cannot be empty.'
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -31,3 +45,10 @@ class BoardType extends AbstractType
         ]);
     }
 }
+            // ajout message erreur si pas de titre
+            // Voir si il faut modifier le "add" de accounts car cela peut créer un nouv account ? (et non le lier a un existant)
+            /*->add('accounts', EntityType::class, [
+                'class' => Account::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])*/
