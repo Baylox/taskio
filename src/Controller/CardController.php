5b6ee0a1d2dac7da502acdf602afef02c06a049e
Baylo <microsoftbaylo@laposte.net>
2025-09-18 17:04:42 +0200

    feat: add ROLE_USER access control to Board and Card controllers

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -13,8 +13,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[IsGranted('ROLE_USER')]
 #[Route('/card')]
 final class CardController extends AbstractController
 {
