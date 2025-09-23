8a1f78be452ec5e9df78a9b50756a060a07e9a76
Baylo <microsoftbaylo@laposte.net>
2025-09-23 11:07:19 +0200

    feat: add HeroComponent with customizable properties and height class method

--- src/Twig/Components/HeroComponent.php
+++ src/Twig/Components/HeroComponent.php
@@ -4,8 +4,8 @@ namespace App\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent]
-final class Hero
+#[AsTwigComponent('Hero')]
+final class HeroComponent
 {
     public string $title = 'The ultra-fast visual manager';
     public ?string $subtitle = 'Organize your tasks, manage your projects, collaborate as a team.';
