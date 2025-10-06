1f9e7878c9d9ba7d49b57b9f9866d4116859e75b
Baylo <microsoftbaylo@laposte.net>
2025-10-06 10:16:14 +0200

    feat: add validation for first and last name fields in registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -20,29 +22,9 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('name', null, [
                 'label' => 'First Name',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter your first name.',
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'First name must be at least {{ limit }} characters long.',
-                        'max' => 50,
-                    ]),
-                ],
             ])
             ->add('lastname', null, [
                 'label' => 'Last Name',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter your last name.',
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'Last name must be at least {{ limit }} characters long.',
-                        'max' => 50,
-                    ]),
-                ],
             ])
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
@@ -59,22 +41,29 @@ class RegistrationFormType extends AbstractType
                 'first_options'  => [
                     'label' => 'Password',
                     'attr' => ['autocomplete' => 'new-password'],
+                    'help' => 'At least 8 characters with uppercase, lowercase, number and special character',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Password must be at least {{ limit }} characters long',
+                        ]),
+                        new Regex([
+                            'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/',
+                            'message' => 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character (@$!%*?&)',
+                        ]),
+                        new NotCompromisedPassword([
+                            'message' => 'This password has been compromised in a data breach. Please choose a different one.',
+                        ]),
+                    ],
                 ],
                 'second_options' => [
                     'label' => 'Repeat Password',
                     'attr' => ['autocomplete' => 'new-password'],
                 ],
                 'invalid_message' => 'The password fields must match.',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        'max' => 4096,
-                    ]),
-                ],
             ])
         ;
     }
